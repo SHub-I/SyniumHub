0c08d6ab4399fc79588afc10975cb0a3f8e1bbfd
SHub-I <elva11gg@gmail.com>
2026-09-21 15:22:34 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,11 +1,13 @@
-local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/jensonhirst/Orion/main/source"))()
+local Rayfield = loadstring(game:HttpGet("https://raw.githubusercontent.com/sirius-software/Rayfield/main/source"))()
 
-local Window = OrionLib:MakeWindow({
+local Window = Rayfield:CreateWindow({
     Name = "Synium Hub",
-    HidePremium = false,
-    SaveConfig = false,
-    IntroEnabled = false,
-    Icon = "rbxassetid://4483345998"
+    LoadingTitle = "Synium Hub",
+    LoadingSubtitle = "by Syn",
+    ConfigurationSaving = false,
+    Discord = {
+        Enabled = false
+    }
 })
 
 shared.SyniumWindow = Window
@@ -13,5 +15,3 @@ shared.SyniumWindow = Window
 local TabManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/tab_manager.lua"))()
 
 TabManager:Load(Window)
-
-OrionLib:Init()
