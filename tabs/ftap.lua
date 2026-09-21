5bd7bd7c1077464c2b15d9aeff6f6860cb2c4c8f
SHub-I <elva11gg@gmail.com>
2026-09-21 15:14:01 +0300

    Update ftap.lua

--- tabs/ftap.lua
+++ tabs/ftap.lua
@@ -1,19 +1,32 @@
---// Synium Hub - FTAP Tab
+return function(Window, rank, Exclusions)
 
-return function(Window)
-    local Tab = Window:CreateTab("FTAP")
-
-    Tab:AddButton({
-        Name = "FTAP Script 1",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/ftap/script1.lua"))()
-        end
+    local Tab = Window:MakeTab({
+        Name = "FTAP",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
     })
 
-    Tab:AddButton({
-        Name = "FTAP Script 2",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/ftap/script2.lua"))()
-        end
+    local Section = Tab:AddSection({
+        Name = "FTAP Scripts"
     })
+
+    local scripts = {
+        {file = "script1.lua", name = "FTAP Script 1"},
+        {file = "script2.lua", name = "FTAP Script 2"}
+    }
+
+    for _, s in ipairs(scripts) do
+        local path = "ftap/" .. s.file
+
+        if not Exclusions:IsExcluded(rank, path) then
+            Tab:AddButton({
+                Name = s.name,
+                Callback = function()
+                    loadstring(game:HttpGet(
+                        "https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/" .. path
+                    ))()
+                end
+            })
+        end
+    end
 end
