849178a7d2c08f4e59ff70ae766334f4f2f67318
SHub-I <elva11gg@gmail.com>
2026-09-21 15:04:19 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,27 +1,25 @@
---// Synium Hub Main
+-- Synium Hub Main
 
--- Load OrionLib if not already loaded
-if not shared.OrionLib then
-    shared.OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shlexware/Orion/main/source"))()
-end
+-- Load Orion Library
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/jensonhirst/Orion/main/source"))()
 
-local OrionLib = shared.OrionLib
-local Icons = shared.SyniumIcons
-local Anim = shared.SyniumAnim
-local TabManager = shared.SyniumTabManager
-
--- Window
+-- Create window
 local Window = OrionLib:MakeWindow({
     Name = "Synium Hub",
     HidePremium = false,
     SaveConfig = false,
     IntroEnabled = false,
-    Icon = Icons.HubIcon
+    Icon = "rbxassetid://4483345998"
 })
 
 shared.SyniumWindow = Window
 
--- Loading animation
+-- Load icons, animations, tab manager
+local Icons = shared.SyniumIcons
+local Anim = shared.SyniumAnim
+local TabManager = shared.SyniumTabManager
+
+-- Play loading animation
 Anim:PlayLoading()
 
 -- Load tabs
@@ -29,3 +27,6 @@ TabManager:Load(Window)
 
 -- Finish loading
 Anim:Finish()
+
+-- REQUIRED for OrionLib
+OrionLib:Init()
