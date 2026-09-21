2b8346ad8a68fe2c50e0858664bd833478636f56
SHub-I <elva11gg@gmail.com>
2026-09-21 15:13:40 +0300

    Update universal.lua

--- tabs/universal.lua
+++ tabs/universal.lua
@@ -1,19 +1,32 @@
---// Synium Hub - Universal Tab
+return function(Window, rank, Exclusions)
 
-return function(Window)
-    local Tab = Window:CreateTab("Universal")
-
-    Tab:AddButton({
-        Name = "Run Script 1",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/universal/script1.lua"))()
-        end
+    local Tab = Window:MakeTab({
+        Name = "Universal",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
     })
 
-    Tab:AddButton({
-        Name = "Run Script 2",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/universal/script2.lua"))()
-        end
+    local Section = Tab:AddSection({
+        Name = "Universal Scripts"
     })
+
+    local scripts = {
+        {file = "script1.lua", name = "Universal Script 1"},
+        {file = "script2.lua", name = "Universal Script 2"}
+    }
+
+    for _, s in ipairs(scripts) do
+        local path = "universal/" .. s.file
+
+        if not Exclusions:IsExcluded(rank, path) then
+            Tab:AddButton({
+                Name = s.name,
+                Callback = function()
+                    loadstring(game:HttpGet(
+                        "https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/" .. path
+                    ))()
+                end
+            })
+        end
+    end
 end
