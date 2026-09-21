0b1a5eaf9e9066f4954643eab6e62b7e5afd1a73
SHub-I <elva11gg@gmail.com>
2026-09-21 15:27:32 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -6,10 +6,4 @@ local function load(path)
     return loadstring(game:HttpGet(base .. path))()
 end
 
--- Core
-load("rank.lua")
-load("rank_exclusions.lua")
-load("tab_manager.lua")
-
--- Main Hub
 load("main.lua")
