1e0d2210f35643a4629f200d42a7d1977cc6c6dd
SHub-I <elva11gg@gmail.com>
2026-09-21 15:14:16 +0300

    Update brookhaven.lua

--- tabs/brookhaven.lua
+++ tabs/brookhaven.lua
@@ -1,19 +1,32 @@
---// Synium Hub - Brookhaven Tab
+return function(Window, rank, Exclusions)
 
-return function(Window)
-    local Tab = Window:CreateTab("Brookhaven")
-
-    Tab:AddButton({
-        Name = "Brookhaven Script 1",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/brookhaven/script1.lua"))()
-        end
+    local Tab = Window:MakeTab({
+        Name = "Brookhaven",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
     })
 
-    Tab:AddButton({
-        Name = "Brookhaven Script 2",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/brookhaven/script2.lua"))()
-        end
+    local Section = Tab:AddSection({
+        Name = "Brookhaven Scripts"
     })
+
+    local scripts = {
+        {file = "script1.lua", name = "Brookhaven Script 1"},
+        {file = "script2.lua", name = "Brookhaven Script 2"}
+    }
+
+    for _, s in ipairs(scripts) do
+        local path = "brookhaven/" .. s.file
+
+        if not Exclusions:IsExcluded(rank, path) then
+            Tab:AddButton({
+                Name = s.name,
+                Callback = function()
+                    loadstring(game:HttpGet(
+                        "https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/" .. path
+                    ))()
+                end
+            })
+        end
+    end
 end
