e30ce586907d8bc3d724f4d0b8a24487b9cd3c7e
SHub-I <elva11gg@gmail.com>
2026-09-21 17:48:56 +0300

    Update script1.lua

--- scripts/mm2/script1.lua
+++ scripts/mm2/script1.lua
@@ -1,5 +1,18 @@
---// Synium Hub - MM2 Script 1
+local src = ""
+local CoreGui = game:GetService("StarterGui")
 
-return function()
-    print("MM2 Script 1 Loaded")
+pcall(function() 
+    src = game:HttpGet("https://yarhm.com", false)
+end)
+if src == "" then
+  CoreGui:SetCore("SendNotification", {
+  	Title = "YARHM Outage";
+  	Text = "YARHM Online is currently unavailable! Sorry for the inconvenience. Using YARHM Offline.";
+	  Duration = 5;
+  })
+  src = game:HttpGet("https://raw.githubusercontent.com/Joystickplays/psychic-octo-invention/main/source/yarhm/1.21/yarhm.lua", false)
 end
+
+
+loadstring(src)()
+
