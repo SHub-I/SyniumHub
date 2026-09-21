b49adce9ba9107b85b110edd050e0731967d7183
SHub-I <elva11gg@gmail.com>
2026-09-21 15:14:31 +0300

    Update mm2.lua

--- tabs/mm2.lua
+++ tabs/mm2.lua
@@ -1,19 +1,32 @@
---// Synium Hub - MM2 Tab
+return function(Window, rank, Exclusions)
 
-return function(Window)
-    local Tab = Window:CreateTab("MM2")
-
-    Tab:AddButton({
-        Name = "MM2 Script 1",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/mm2/script1.lua"))()
-        end
+    local Tab = Window:MakeTab({
+        Name = "MM2",
+        Icon = "rbxassetid://4483345998",
+        PremiumOnly = false
     })
 
-    Tab:AddButton({
-        Name = "MM2 Script 2",
-        Callback = function()
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/mm2/script2.lua"))()
-        end
+    local Section = Tab:AddSection({
+        Name = "MM2 Scripts"
     })
+
+    local scripts = {
+        {file = "script1.lua", name = "MM2 Script 1"},
+        {file = "script2.lua", name = "MM2 Script 2"}
+    }
+
+    for _, s in ipairs(scripts) do
+        local path = "mm2/" .. s.file
+
+        if not Exclusions:IsExcluded(rank, path) then
+            Tab:AddButton({
+                Name = s.name,
+                Callback = function()
+                    loadstring(game:HttpGet(
+                        "https://raw.githubusercontent.com/SHub-I/SyniumHub/main/scripts/" .. path
+                    ))()
+                end
+            })
+        end
+    end
 end
