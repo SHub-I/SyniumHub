ac43a927e9e260eab495ff20dc0464c7a174d751
SHub-I <elva11gg@gmail.com>
2026-09-21 17:39:39 +0300

    Update tab_manager.lua

--- tab_manager.lua
+++ tab_manager.lua
@@ -20,7 +20,7 @@ function Manager:Load(Window)
     end
 
     pcall(function() auto(Window, "None", {}, "empty") end)
-    pcall(function() auto(Window, "None", {}, "universal") end)
+    pcall(function() auto(Window, "None", {}, "universl") end)
     pcall(function() auto(Window, "None", {}, "ftap") end)
     pcall(function() auto(Window, "None", {}, "brookhaven") end)
     pcall(function() auto(Window, "None", {}, "mm2") end)
