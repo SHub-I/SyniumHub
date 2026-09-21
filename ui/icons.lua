47664c70544ad6d711a4a1420756a0bb737e2baf
SHub-I <elva11gg@gmail.com>
2026-09-21 14:39:30 +0300

    Create icons.lua

---
+++ ui/icons.lua
@@ -0,0 +1,11 @@
+local Icons = {
+    HubIcon = "rbxassetid://17591594269",
+
+    Universal = "rbxassetid://17591594269",
+    FTAP = "rbxassetid://17591594269",
+    Brookhaven = "rbxassetid://17591594269",
+    MM2 = "rbxassetid://17591594269"
+}
+
+shared.SyniumIcons = Icons
+return Icons
