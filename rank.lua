5ee6b93a148b2411b348899ec886d6be5460b53b
SHub-I <elva11gg@gmail.com>
2026-09-21 15:10:13 +0300

    Update rank.lua

--- rank.lua
+++ rank.lua
@@ -1,20 +1,13 @@
---// Synium Hub Rank System
-
 local HttpService = game:GetService("HttpService")
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
 
-local url = "https://raw.githubusercontent.com/SHub-I/SyniumHub/main/config/players.json"
+local players = HttpService:JSONDecode(
+    game:HttpGet("https://raw.githubusercontent.com/SHub-I/SyniumHub/main/config/players.json")
+)
 
-local function fetch()
-    local raw = game:HttpGet(url)
-    return HttpService:JSONDecode(raw)
+local Rank = {}
+
+function Rank:GetRank()
+    return players[game.Players.LocalPlayer.Name] or "None"
 end
 
-local data = fetch()
-
--- If player not listed → "None"
-local rank = data[LocalPlayer.Name] or "None"
-
-shared.SyniumRank = rank
-return rank
+return Rank
