37e4a7b249dce7a2186ef06e56c8dcecabdaf36b
SHub-I <elva11gg@gmail.com>
2026-09-21 14:31:04 +0300

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,26 @@
+--// Synium Hub Loader
+local base = "https://raw.githubusercontent.com/yourname/synium-hub/main/"
+
+local function load(path)
+    return loadstring(game:HttpGet(base .. path))()
+end
+
+-- Core
+load("core/orion_init.lua")
+load("core/rank.lua")
+load("core/rank_exclusions.lua")
+load("core/tab_manager.lua")
+
+-- UI
+load("ui/loading_screen.lua")
+load("ui/icons.lua")
+load("ui/animations.lua")
+
+-- Main Hub
+load("main.lua")
+
+-- Tabs
+load("tabs/universal.lua")
+load("tabs/ftap.lua")
+load("tabs/brookhaven.lua")
+load("tabs/mm2.lua")
