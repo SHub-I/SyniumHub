3a72889078e2c7e91441ee60fd4b529c049a0ddb
SHub-I <elva11gg@gmail.com>
2026-09-21 15:25:48 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,5 @@
---// Synium Hub Loader
+-- Synium Hub Loader (Rayfield)
+
 local base = "https://raw.githubusercontent.com/SHub-I/SyniumHub/main/"
 
 local function load(path)
@@ -6,21 +7,9 @@ local function load(path)
 end
 
 -- Core
-load("core/orion_init.lua")
-load("core/rank.lua")
-load("core/rank_exclusions.lua")
-load("core/tab_manager.lua")
-
--- UI
-load("ui/loading_screen.lua")
-load("ui/icons.lua")
-load("ui/animations.lua")
+load("rank.lua")
+load("rank_exclusions.lua")
+load("tab_manager.lua")
 
 -- Main Hub
 load("main.lua")
-
--- Tabs
-load("tabs/universal.lua")
-load("tabs/ftap.lua")
-load("tabs/brookhaven.lua")
-load("tabs/mm2.lua")
