da1bce64d950b1cedd6d4a871fd69be0c1951881
SHub-I <elva11gg@gmail.com>
2026-09-22 16:08:57 +0300

    asdd

--- scripts/ftap/example.lua
+++ scripts/ftap/example.lua
@@ -1,1 +1,6 @@
+-- Auto-registering script template for Synium Hub
 
+Register("Infinite Yield", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+    print("Running:", "Infinite Yield")
+end)
