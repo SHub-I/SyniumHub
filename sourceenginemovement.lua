faa9bc0a2d8e6c357217291e1ac59ebf403293c2
SHub-I <elva11gg@gmail.com>
2026-09-26 17:02:17 +0300

    asdas

--- sourceenginemovement.lua
+++ sourceenginemovement.lua
@@ -30,9 +30,9 @@ local Config = {
     AIR_ACCEL = 2200,
 
     AIR_SPEED = 6,
-    RUN_SPEED = 20,
-    WALK_SPEED = 20,
-    CROUCH_SPEED = 20,
+    RUN_SPEED = 21,
+    WALK_SPEED = 21,
+    CROUCH_SPEED = 21,
 
     AIR_MAX_SPEED = 36.5,
     AIR_MAX_SPEED_FRIC = 3,
