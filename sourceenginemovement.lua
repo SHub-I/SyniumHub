6a6395d8466d9665ae0bb8875ea5a6f043e7ad7e
SHub-I <elva11gg@gmail.com>
2026-09-26 17:03:49 +0300

    sdsa

--- sourceenginemovement.lua
+++ sourceenginemovement.lua
@@ -30,9 +30,9 @@ local Config = {
     AIR_ACCEL = 2200,
 
     AIR_SPEED = 6,
-    RUN_SPEED = 21,
-    WALK_SPEED = 21,
-    CROUCH_SPEED = 21,
+    RUN_SPEED = 18,
+    WALK_SPEED = 18,
+    CROUCH_SPEED = 18,
 
     AIR_MAX_SPEED = 36.5,
     AIR_MAX_SPEED_FRIC = 3,
